01dfc628f850c4805e904ada15ab0857946c785e
rychuhardy <ryszard.sikora7@gmail.com>
2015-10-24 14:05:02 +0200

    Implemendted threads

--- main.cpp
+++ main.cpp
@@ -26,55 +26,69 @@ mutex url_mutex;
 
 void load_url_address()
 {
-    static sql_model model;
+    static int counter;
+    sql_model model;
     while(true) {
-        string result = model.get_next_url_address();
+        string result = model.get_next_url_address();       //model->connect()
         unique_lock<mutex> lock {url_mutex};
         url_addresses.push(result);
+        ++counter;
+        int b = url_addresses.size(); // temp
         lock.unlock();
         url_condition.notify_one();
-        while(url_addresses.size() >= model.get_rows_number()) {
+        //int size = url_addresses.size();
+       // while(size >= model.get_rows_number()) {
+        //    size=url_addresses.size();
+        //    url_condition.wait(lock);
+//            url_condition.notify_one();
+        if(counter == model.get_rows_number()) {
+            counter =0 ;
+            this_thread::sleep_for(seconds(10));  //model->disconnect()
+        }
+       // }
+    }
+
+}
+void acquire_url() {
+    string url;
+    while (true) {
+        unique_lock<mutex> lock{url_mutex};
+        while (url_addresses.empty()) {
             url_condition.wait(lock);
         }
-    }
-
-}
-void acquire_url()
-{
-    string url;
-    while(true) {
-        unique_lock<mutex> lock{url_mutex};
-        while(url_addresses.empty()) {
-                url_condition.wait(lock);
-        }
-            url = url_addresses.front();
-            url_addresses.pop();
+        url = url_addresses.front();
+        url_addresses.pop();
 //            if(lock.owns_lock()) {
-                lock.unlock();
+        lock.unlock();
 //            }
-            url_condition.notify_one();
-
-        thread t {&scrape_webpage, url.c_str()};
-        t.detach();
+      //  url_condition.notify_one();
+        //thread t(scraperewebpage)
+        // t.detach;
+        scrape_webpage(url.c_str());
     }
 }
+
 void scrape_webpage(const char* link)
 {
-    string str = link;
-    python_scraper scraper(str.c_str());
-    string result = scraper.scrape();
-
+     string str = link;
+     python_scraper scraper(str.c_str());
+     string result = scraper.scrape();
+    cout << result << endl;
     //call parser(result)
 }
 
 int main(int argc, char** argv)
 {
 
-    thread t1 {load_url_address};
-    this_thread::sleep_for(std::chrono::milliseconds(300));
-    thread t2 {acquire_url};
-    t1.join();
-    t2.join();
-
+//    thread t1 {load_url_address};
+//    this_thread::sleep_for(std::chrono::milliseconds(300));
+//    thread t2 {acquire_url};
+//    t1.join();
+//    t2.join();
+    sql_model model;
+    model.
+//    char* name = "http://www.google.pl";
+//    python_scraper scraper(name);
+//    scraper.scrape();
     return 0;
 }
