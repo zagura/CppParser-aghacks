871d5049fc6b5457f71ea5708e589fda88fb9e07
rychuhardy <ryszard.sikora7@gmail.com>
2015-10-24 02:33:40 +0200

    Implemented insert_row in sql_manager and added column_name and table_name arguments to insert_row and get_row_by_line_number

--- sql_manager.cpp
+++ sql_manager.cpp
@@ -7,7 +7,7 @@
 
 sql_manager::sql_manager(): database_address{"localhost"}, database_username{"test_user"}, database_password{"123abc"} {
 
-    driver = driver = sql::mysql::get_mysql_driver_instance();
+    driver = sql::mysql::get_mysql_driver_instance();
     connection = driver -> connect(database_address,database_username,database_password);
 }
 
@@ -15,13 +15,17 @@ sql_manager::~sql_manager() {
     delete connection;
 }
 
-const string sql_manager::get_row_by_line_number(const string line, const string column_name) {
+const string sql_manager::get_row_by_line_number(const string line, const string column_name, const string table_name) {
     unique_ptr<sql::Statement> statement {connection->createStatement()};
     unique_ptr<sql::ResultSet> result { (statement->
-            executeQuery("SELECT "+column_name+"FROM restaurants ORDER BY updated_at LIMIT "+line+",1"))}; //TO DEBUG
+            executeQuery("SELECT "+column_name+"FROM "+table_name+" ORDER BY updated_at LIMIT "+line+",1"))}; //TO DEBUG
     return result->getString("url").asStdString();
 }
 
-void sql_manager::insert_row(const string &column_name, const string &text_inserted) {
-
+void sql_manager::insert_row(const string table_name,const string column_name, const string &text_inserted) {
+    unique_ptr<sql::PreparedStatement> statement {connection->prepareStatement
+            ("INSERT INTO "+table_name+" ("+column_name+") VALUES (?)"
+                    " ON DUPLCATE KEY UPDATE "+column_name+"=VALUES("+column_name+")")};
+    statement->setString(1,text_inserted);
+    statement->execute();
 }
