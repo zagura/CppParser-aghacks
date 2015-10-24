8852d5249e5809563cb1c3fd8e37dc741a18e17e
Michal <zagura6@gmail.com>
2015-10-24 15:15:30 +0200

    Adding parser interface

---
+++ Parser/parser.h
@@ -0,0 +1,23 @@
+//
+// Created by misiek on 24.10.15.
+//
+
+#ifndef AGHACKS_PARSER_H
+#define AGHACKS_PARSER_H
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::string;
+
+class parser {
+public:
+    parser(string content);
+    string get_unformatted_text();
+    vector <string> get_meals();
+    ~parser();
+    void parse_html(string text);
+};
+
+
+#endif //AGHACKS_PARSER_H
