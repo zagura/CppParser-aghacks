db13a4a8cb8af9e73b6d94be81b2491d6a50d4b8
Michal <zagura6@gmail.com>
2015-10-24 09:55:55 +0200

    makefile cleanup

--- main.cpp
+++ main.cpp
@@ -1,9 +1,82 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include "sql_manager.h"
+#include "sql_model.h"
+#include "python_scraper.h"
+#include <thread>
+#include <future>
+#include <chrono>
+#include <queue>
+#include <condition_variable>
+#include <mutex>
 
 using namespace std;
+using namespace std::chrono;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+
+void acquire_url();
+void load_url_address();
+void scrape_webpage();
+
+
+queue<string> url_addresses{};
+condition_variable url_condition;
+mutex url_mutex;
+
+void load_url_address()
+{
+    static sql_model model;
+    while(true) {
+        string result = model.get_next_url_address();
+        unique_lock<mutex> lock {url_mutex};
+        url_addresses.push(result);
+        url_condition.notify_one();
+        if(url_addresses.size() >= model.get_rows_number()) {
+            url_condition.wait(lock);
+        }
+    }
+
+}
+void acquire_url()
+{
+    string url;
+    while(true) {
+        unique_lock<mutex> lock{};
+        while(url_addresses.empty()) {
+            url_condition.wait(lock);
+        }
+            url = url_addresses.front();
+            url_addresses.pop();
+            lock.unlock();
+            url_condition.notify_one();
+
+//        thread t {scrape_webpage, url.c_str()};
+    }
+}
+void scrape_webpage(const char* link )
+{
+    python_scraper scraper(link);
+    string result = scraper.scrape();
 }
 
+int main(int argc, char** argv)
+{
+
+//    while(true) {
+//        promise<string> url_promise;
+//        auto url_future = url_promise.get_future();
+//        thread url_thread {get_url_address, url_promise};
+//        url_thread.join();
+//        string url_result = url_future.get();
+//
+//        promise<string> scraper_promise;
+//        auto scraper_future = scraper_promise.get_future();
+//        thread scraper_thread {scrape_webpage, argv[1], argv[2],url_result};
+//
+//        this_thread::sleep_for(duration_cast<milliseconds>(seconds(4)));
+//
+//    }
+
+    return 0;
+}
